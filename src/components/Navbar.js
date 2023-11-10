f3df4e7243842f29b0d9401120c86a3ed88e0c31
Manjunath Kalburgi <1643278+kalburgimanjunath@users.noreply.github.com>
2023-11-10 14:37:25 +0000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,6 +10,7 @@ export default function Navbar() {
     { title: "Contact Me", link: "contact" },
   ];
   const [active, setActive] = useState("");
+  const [showSearch, setSearch] = useState(false);
 
   return (
     <div className="flex justify-center fixed top-0 shadow-lg w-full bg-white">
@@ -31,8 +32,22 @@ export default function Navbar() {
                 </li>
               );
             })}
+          <li className="p-2 cursor-pointer">
+            <svg
+              onClick={() => setSearch(!showSearch)}
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              viewBox="0 0 24 24"
+              class="w-6 h-6"
+            >
+              <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+            </svg>
+          </li>
         </ul>
-        <Search />
+        {showSearch && <Search />}
       </nav>
     </div>
   );
