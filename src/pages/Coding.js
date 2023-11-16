38a73a17d4cb03b97df89e1b6785eb2d09afa423
Manjunath Kalburgi <1643278+kalburgimanjunath@users.noreply.github.com>
2023-11-16 07:31:05 +0000

    Update Coding.js

--- src/pages/Coding.js
+++ src/pages/Coding.js
@@ -25,11 +25,26 @@ const Sidebar = () => {
 export default function Coding() {
   const { id } = useParams();
   return (
-    <div className="mt-20 ml-0 grid grid-cols-2">
+    <div className="mt-5 ml-0 grid grid-cols-2 overflow-auto">
       <div className="fixed mt-15 pt-1 left-0 bottom-0 bg-gray-300 text-gray-100 top-0 w-3/2">
         <Sidebar />
       </div>
-      <div className="m-20">{id}</div>
+      <div className="ml-20 w-screen">
+        {id}
+        <iframe
+          src="https://codesandbox.io/p/github/kalburgimanjunath/next-sandbox/main?embed=1&showConsole=true"
+          style={{
+            width: "100%",
+            height: "500px",
+            border: 0,
+            borderRadius: "4px",
+            overflow: "hidden",
+          }}
+          title="kalburgimanjunath/next-sandbox/main"
+          allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+          sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+        ></iframe>
+      </div>
     </div>
   );
 }
