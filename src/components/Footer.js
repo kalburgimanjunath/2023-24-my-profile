90570d40dd00b52758acfbaa5a8c64cad9448be5
Manjunath Kalburgi <1643278+kalburgimanjunath@users.noreply.github.com>
2023-12-19 07:57:24 +0000

    Update Footer.js and yarn.lock

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 export default function Footer() {
   return (
-    <div className="fixed mt-2 p-2 shadow-lg bottom-0 bg-black text-white w-full right-0 left-0 justify-center text-center">
+    <div className="mt-2 p-2 shadow-lg bottom-0 bg-black text-white w-full right-0 left-0 justify-center text-center">
       This website is developed by Manjunath Kalburgi and &copy; 2023 rights are
       reserved .
     </div>
